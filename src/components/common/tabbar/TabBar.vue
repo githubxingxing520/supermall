1a8988229e5b3e6c78777d1df7d1f8107e570dea
githubxingxing520 <121507664@qq.com>
2020-05-25 22:30:51 +0800

    完成tabbar封装

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="tabbar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+#tabbar {
+  display: flex;
+  align-items: center;
+  background-color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tabbar_item"
+       @click="handleItemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="styleColor">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activeColor: {
+      type: String,
+      default: "red"
+    },
+    path: {
+      type: String,
+      default: ""
+    }
+  },
+  data () {
+    return {
+
+    };
+  },
+  computed: {
+    isActive () {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    styleColor () {
+      return this.isActive ? { color: this.activeColor } : {}
+    }
+  },
+  watch: {
+
+  },
+  methods: {
+    // 点击tab-item
+    handleItemClick () {
+      this.$router.replace(this.path)
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.tabbar_item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+  img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+  .active {
+    color: red;
+  }
+}
+</style>
